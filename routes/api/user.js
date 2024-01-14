485e83044318e0b995ef0ad3dad634a7ce1c18f3
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2024-01-14 21:12:13 +0200

    current

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const { authenticate } = require("../../middlewares");
 
-// const { userSchemas } = require("../../models/user");
-
 const ctrl = require("../../controllers/user");
 
 const router = express.Router();
