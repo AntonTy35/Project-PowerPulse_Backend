59b6cc980f3a9e44ecd4c4bd804b7312f690316a
EduardMLT <eddku@i.ua>
2024-01-14 14:52:23 +0200

    initial edd_02

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -1,12 +1,12 @@
-// const { updateUser } = require("./updateUser");
-const {productsAll} = require("./productsAll");
+const { productsCategories } = require("./productsCategories");
+const { productsAll } = require("./productsAll");
 const { productsRecommended } = require("./productsRecommended");
 const { productsNotRecommended } = require("./productsNotRecommended");
 
 const { ctrlWrapper } = require("../../helpers");
 
 module.exports = {
-//   updateUser: ctrlWrapper(updateUser),
+  productsCategories: ctrlWrapper(productsCategories),
   productsAll: ctrlWrapper(productsAll),
   productsRecommended: ctrlWrapper(productsRecommended),
   productsNotRecommended: ctrlWrapper(productsNotRecommended),
