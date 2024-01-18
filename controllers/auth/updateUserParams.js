603b099a20d2331a6916e3bf2b677c106971428d
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2024-01-18 16:11:58 +0200

    updateUserParams_new

--- controllers/auth/updateUserParams.js
+++ controllers/auth/updateUserParams.js
@@ -4,19 +4,16 @@ const calculateBMR = require("../../utils/calculateBMR");
 const updateUserParams = async (req, res) => {
   try {
     const { email } = req.user;
-    const userParams = { ...req.body };
 
-    const updatedUser = await User.findOneAndUpdate(
-      { email },
-      { userParams },
-      { new: true }
-    );
+    const updatedUser = await User.findOneAndUpdate({ email }, req.body, {
+      new: true,
+    });
 
     if (!updatedUser) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    const { desiredWeight, height, birthday, sex, levelActivity } = userParams;
+    const { desiredWeight, height, birthday, sex, levelActivity } = updatedUser;
     const bmr = calculateBMR(
       desiredWeight,
       height,
@@ -28,7 +25,14 @@ const updateUserParams = async (req, res) => {
     res.status(200).json({
       user: {
         name: updatedUser.name,
-        userParams: updatedUser.userParams,
+        email: updatedUser.email,
+        height: updatedUser.height,
+        currentWeight: updatedUser.currentWeight,
+        desiredWeight: updatedUser.desiredWeight,
+        birthday: updatedUser.birthday,
+        blood: updatedUser.blood,
+        sex: updatedUser.sex,
+        levelActivity: updatedUser.levelActivity,
       },
       bmr,
     });
