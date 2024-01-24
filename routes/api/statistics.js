e457aa057683d9a92dacbd4c68731aae617d8dc1
AntonTy35 <123330468+AntonTy35@users.noreply.github.com>
2024-01-24 17:19:32 +0200

    Update statistics.js

--- routes/api/statistics.js
+++ routes/api/statistics.js
@@ -5,6 +5,6 @@ const ctrl = require("../../controllers/statistics");
 const jsonParser = express.json();
 const router = express.Router();
 
-router.get("/statisticsUser", ctrl.createStatistics);
+router.get("/statistics", ctrl.createStatistics);
 
 module.exports = router;
